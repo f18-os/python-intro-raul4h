4510b16989b8bb91077a886407b97b58e4b0105c
raul4h <rjhinostrozagarcia@miners.utep.edu>
2018-09-19 20:18:09 -0600

    Import p5

--- shell/shell.py
+++ shell/shell.py
@@ -49,6 +49,53 @@ def p4(inp):
         childPidCode = os.wait()
         #os.write(1, ("Parent: Child %d terminated with exit code %d\n" % 
         #             childPidCode).encode())
+        
+def piping():
+    pid = os.getpid()               # get and remember pid
+
+    pr,pw = os.pipe()
+    for f in (pr, pw):
+        os.set_inheritable(f, True)
+    print("pipe fds: pr=%d, pw=%d" % (pr, pw))
+
+    import fileinput
+
+    print("About to fork (pid=%d)" % pid)
+
+    rc = os.fork()
+
+    if rc < 0:
+        print("fork failed, returning %d\n" % rc, file=sys.stderr)
+        sys.exit(1)
+
+    elif rc == 0:                   #  child - will write to pipe
+        print("Child: My pid==%d.  Parent's pid=%d" % (os.getpid(), pid), file=sys.stderr)
+        args = ["wc", "p3-exec.py"]
+
+        os.close(1)                 # redirect child's stdout
+        os.dup(pw)
+        for fd in (pr, pw):
+            os.close(fd)
+        for dir in re.split(":", os.environ['PATH']): # try each directory in path
+            program = "%s/%s" % (dir, inp[0])
+            try:
+                os.execve(program,inp,os.environ) # try to exec program
+            except FileNotFoundError:             # ...expected
+                pass                              # ...fail quietly 
+
+    else:                           # parent (forked ok)
+        print("Parent: My pid==%d.  Child's pid=%d" % (os.getpid(), rc), file=sys.stderr)
+        os.close(0)
+        os.dup(pr)
+        for fd in (pw, pr):
+            os.close(fd)
+        for line in fileinput.input():
+           for dir in re.split(":", os.environ['PATH']): # try each directory in path
+            program = "%s/%s" % (dir, inp[0])
+            try:
+                os.execve(program,inp,os.environ) # try to exec program
+            except FileNotFoundError:             # ...expected
+                pass                              # ...fail quietly 
 
 inp = input("$ ")
 ex = 0
